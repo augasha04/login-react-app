432a439d65b4e7313aa477c76f3fa3beb574f1c2
Aftermath001 <alvinasiachi@gmail.com>
2023-04-18 14:32:05 +0300

    LanndingP

---
+++ src/landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import LogIn from './login.js';
+// function Welcome(props) {
+//     return <h2>I am a { props.Name }!</h2>;
+//   }
+  
+  function Greeting() {
+    // const userName = `${Name}`;
+    return (
+      <>
+          <h1>Welcome to Moringa</h1>
+          {/* console.log${Name}; */}
+      </>
+    );
+  }
+  export default Greeting;
--- src/login.js
+++ src/login.js
@@ -1,20 +1,28 @@
-import React from "react";
-
-function LogIn(){
+import React, { useState } from "react";
+function LogIn() {
+    const [name, setName] = useState("");
+    function handleSubmit(event) {
+      event.preventDefault();
+      alert(`Welcome, ${name}!`);
+    }
+    function handleNameChange(event) {
+      setName(event.target.value);
+    }
     return (
-     <form>
-     <div className = "Name">
-     <label>Enter your name:
-           <input type="text" />
-         </label>
-         <label>Enter your Email:
-           <input type="email" />
-         </label>
-         <button>
-         <input type="submit" />
-         </button>
-     </div>
-    </form>
-   )}; 
+        <form onSubmit={handleSubmit}>
+          <div className="Name">
+            <label>
+              Enter your name:
+              <input type="text" value={name} onChange={handleNameChange} />
+            </label>
+            <label>
+              Enter your Email:
+              <input type="email" />
+            </label>
+            <input type="submit" />
+          </div>
+        </form>
+      );
+    }
 
 export default LogIn;
